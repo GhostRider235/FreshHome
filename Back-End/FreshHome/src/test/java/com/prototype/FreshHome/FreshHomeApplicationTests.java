6d3502657dee3473c09bab62c141df50ad378c72
GhostRider235 <luchomaniaco@outlook.com>
2024-10-28 11:00:13 -0500

    Creacion de proyecto primera fase.

---
+++ Back-End/FreshHome/src/test/java/com/prototype/FreshHome/FreshHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototype.FreshHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
