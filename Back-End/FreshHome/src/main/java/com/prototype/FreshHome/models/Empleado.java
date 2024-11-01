fd979829dc612691e6d65c8899e30cd09eac5744
GhostRider235 <luchomaniaco@outlook.com>
2024-11-01 17:12:47 -0500

    Revert "Solo se utilizara Stpring Boot"
    This reverts commit a9a6669ed3b807d3d7c33b4596e66b71ea04fccb.

---
+++ Back-End/FreshHome/src/main/java/com/prototype/FreshHome/models/Empleado.java
@@ -0,0 +1,13 @@
+package com.prototype.FreshHome.models;
+
+import java.time.LocalDate;
+
+public class Empleado {
+    private String IdEmpleado;
+    private String nombreEmpleado;
+    private String correo;
+    private LocalDate FechaNacimiento;
+    private short añosExperiencia;
+    private short edad;
+    private int CalificacionEmpleado;
+}
